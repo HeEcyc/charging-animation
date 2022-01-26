f46c2e8010349e95b6e3178f99eb1dcbe6152e90
MMCXD MMCXD <mmcxdgit@gmail.com>
2022-01-26 18:22:54 +0200

    Implemented ironSource SDK

--- app/src/main/java/com/charginging/animationation/base/BaseActivity.kt
+++ app/src/main/java/com/charginging/animationation/base/BaseActivity.kt
@@ -12,15 +12,30 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import com.charginging.animationation.BR
 import com.charginging.animationation.utils.APP_LINK
+import com.charginging.animationation.utils.IRON_SOURCE_APP_KEY
+import com.ironsource.mediationsdk.IronSource
 
 abstract class BaseActivity<TViewModel : BaseViewModel, TBinding : ViewDataBinding> :
     AppCompatActivity() {
+
+    override fun onResume() {
+        super.onResume()
+        IronSource.onResume(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        IronSource.onPause(this)
+    }
+
     private var onPermission: ((Boolean) -> Unit)? = null
 
     lateinit var binding: TBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        IronSource.setMetaData("is_child_directed","false")
+        IronSource.init(this, IRON_SOURCE_APP_KEY)
         StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder().build())
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         binding = DataBindingUtil.setContentView(this, provideLayoutId())
--- app/src/main/java/com/charginging/animationation/utils/ConstantValues.kt
+++ app/src/main/java/com/charginging/animationation/utils/ConstantValues.kt
@@ -6,3 +6,4 @@ val APP_LINK = "https://play.google.com/store/apps/details?id=" + App.instance.p
 
 const val APPSFLYER_DEV_KEY = "RkUFwFyHatoPGg2ajYrhmY"
 const val APP_METRICA_API_KEY = "42c13bdf-8a9a-4a60-997c-3da01f56e924"
+const val IRON_SOURCE_APP_KEY = "1335d8ee1"
