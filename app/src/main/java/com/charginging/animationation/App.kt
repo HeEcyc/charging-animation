98827689160683ed9ff922f9a6f650ff0b4549cf
MMCXD MMCXD <mmcxdgit@gmail.com>
2022-01-26 17:36:09 +0200

    Implemented AppMetrica SDK

--- app/src/main/java/com/charginging/animationation/App.kt
+++ app/src/main/java/com/charginging/animationation/App.kt
@@ -1,6 +1,10 @@
 package com.charginging.animationation
 
 import android.app.Application
+import android.provider.Settings
+import com.charginging.animationation.utils.APP_METRICA_API_KEY
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 
 class App : Application() {
 
@@ -10,7 +14,26 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        activateAppMetrica()
         instance = this
+    }
+
+    private fun activateAppMetrica() {
+        val appMetricaConfig: YandexMetricaConfig =
+            YandexMetricaConfig.newConfigBuilder(APP_METRICA_API_KEY)
+                .handleFirstActivationAsUpdate(isFirstActivationAsUpdate())
+                .withLocationTracking(true)
+                .withLogs()
+                .withStatisticsSending(true)
+                .build()
+        YandexMetrica.activate(applicationContext, appMetricaConfig)
+    }
+
+    private fun isFirstActivationAsUpdate(): Boolean {
+        // Implement logic to detect whether the app is opening for the first time.
+        // For example, you can check for files (settings, databases, and so on),
+        // which the app creates on its first launch.
+        return Settings.canDrawOverlays(this)
     }
 
 }
--- app/src/main/java/com/charginging/animationation/utils/ConstantValues.kt
+++ app/src/main/java/com/charginging/animationation/utils/ConstantValues.kt
@@ -3,4 +3,6 @@ package com.charginging.animationation.utils
 import com.charginging.animationation.App
 
 val APP_LINK = "https://play.google.com/store/apps/details?id=" + App.instance.packageName
+
 const val APPSFLYER_DEV_KEY = "RkUFwFyHatoPGg2ajYrhmY"
+const val APP_METRICA_API_KEY = "42c13bdf-8a9a-4a60-997c-3da01f56e924"
