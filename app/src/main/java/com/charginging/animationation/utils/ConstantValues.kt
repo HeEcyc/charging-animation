3f5b4e588eb81281929b837eaab459c69a4a3b97
Nikita <niklog21@gmail.com>
2022-03-29 12:11:09 +0300

    Added new IronSource app key

--- app/src/main/java/com/charginging/animationation/utils/ConstantValues.kt
+++ app/src/main/java/com/charginging/animationation/utils/ConstantValues.kt
@@ -5,4 +5,4 @@ import com.charginging.animationation.App
 val APP_LINK = "https://play.google.com/store/apps/details?id=" + App.instance.packageName
 
 const val APPSFLYER_DEV_KEY = "RkUFwFyHatoPGg2ajYrhmY"
-const val IRON_SOURCE_APP_KEY = "1335d8ee1"
+const val IRON_SOURCE_APP_KEY = "1428fe259"
