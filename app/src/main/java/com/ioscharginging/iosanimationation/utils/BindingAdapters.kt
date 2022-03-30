752ba160a62cc95a8cea8495e6e9ced88ac27788
Nikita <niklog21@gmail.com>
2022-03-30 16:45:22 +0300

    Changed settings activity

--- app/src/main/java/com/ioscharginging/iosanimationation/utils/BindingAdapters.kt
+++ app/src/main/java/com/ioscharginging/iosanimationation/utils/BindingAdapters.kt
@@ -1,16 +1,15 @@
 package com.ioscharginging.iosanimationation.utils
 
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-
-@BindingAdapter("layout_constraintHorizontal_bias")
-fun setHorizontalBias(v: View, b: Float) {
-    (v.layoutParams as ConstraintLayout.LayoutParams).horizontalBias = b
-}
+import vn.luongvo.widget.iosswitchview.SwitchView
 
 @BindingAdapter("adapter")
 fun setRecyclerViewAdapter(rv: RecyclerView, a: RecyclerView.Adapter<*>) {
     rv.adapter = a
+}
+
+@BindingAdapter("checked")
+fun setChecked(sv: SwitchView, b: Boolean) {
+    sv.toggle(b)
 }
