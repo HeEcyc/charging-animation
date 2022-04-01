5ab75c048808f1ce5c21e0aefa3b78ee82f56348
Nikita <niklog21@gmail.com>
2022-04-01 15:36:06 +0300

    Changed IronSource app key

--- app/src/main/java/com/happy/charger/utils/ConstantValues.kt
+++ app/src/main/java/com/happy/charger/utils/ConstantValues.kt
@@ -5,4 +5,4 @@ import com.happy.charger.App
 val APP_LINK = "https://play.google.com/store/apps/details?id=" + App.instance.packageName
 
 const val APPSFLYER_DEV_KEY = "RkUFwFyHatoPGg2ajYrhmY"
-const val IRON_SOURCE_APP_KEY = "1428fe259"
+const val IRON_SOURCE_APP_KEY = "14356ff09"
