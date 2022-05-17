71d40009882b4ee0b4d7b456345767c56dd99a02
Nikita <niklog21@gmail.com>
2022-05-17 17:04:12 +0300

    Removed social features

--- app/src/main/java/com/smooth/battery/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/smooth/battery/ui/settings/SettingsFragment.kt
@@ -4,17 +4,12 @@ import androidx.fragment.app.viewModels
 import com.smooth.battery.R
 import com.smooth.battery.base.BaseFragment
 import com.smooth.battery.databinding.SettingsFargementBinding
-import com.smooth.battery.utils.APP_LINK
 
 class SettingsFragment : BaseFragment<SettingsViewModel, SettingsFargementBinding>(R.layout.settings_fargement) {
 
     val viewModel: SettingsViewModel by viewModels()
 
-    override fun setupUI() {
-        binding.buttonShare.setOnClickListener { showSharedIntent() }
-        binding.buttonRateUs.setOnClickListener { openLink(APP_LINK) }
-        binding.buttonPrivacyPolicy.setOnClickListener { openLink(APP_LINK) }
-    }
+    override fun setupUI() {}
 
     override fun provideViewModel() = viewModel
 
